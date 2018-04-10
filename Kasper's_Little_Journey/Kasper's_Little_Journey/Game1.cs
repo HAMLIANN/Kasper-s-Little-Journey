2a111a603ba30716ab936ff3149ab4d8d30a705c
Lancel0t790 <emilkim2489@gmail.com>
2018-04-10 15:45:01 +0200

    sd

--- Kasper's_Little_Journey/Kasper's_Little_Journey/Game1.cs
+++ Kasper's_Little_Journey/Kasper's_Little_Journey/Game1.cs
@@ -190,6 +190,11 @@ namespace Kasper_s_Little_Journey
 				enemyList.Add(new Enemy(Content.Load<Texture2D>("EliasHead"), new Vector2(randX, randY), Content.Load<Texture2D>("EnemyPen")));
 			}
 
+			if (hud.playerScore >= 1000 && hud.playerScore <= 1010)
+			{
+				enemyList.Add(new Enemy(Content.Load<Texture2D>("VendelaHead"), new Vector2(randX, randY), Content.Load<Texture2D>("EnemyPen")));
+			}
+
 			// if any of the Enemy in the list were destroyed(or invisible), then remove them from the list
 			for (int i = 0; i < enemyList.Count; i++)
 			{
